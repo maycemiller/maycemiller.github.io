2bc4a23d3d459286af1c3812933263d3a207de06
maycemiller <mami8235@colorado.edu>
2023-11-07 12:49:15 -0700

    in class activity 11

---
+++ ica/week11.js
@@ -0,0 +1,47 @@
+console.log("test");
+
+const x = 1;
+const y = 2;
+
+if (x===y) {
+    console.log("Success");
+}
+else {
+    console.log("Fail");
+}
+
+const btn = document.querySelector("button");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+document.addEventListener("DOMContentLoaded", () => {
+    canvas.width = document.documentElement.clientWidth;
+    canvas.height = document.documentElement.clientHeight;
+  });
+  
+function random(number) {
+    return Math.floor(Math.random()* number);
+}
+
+//console.log(random(2));
+
+
+
+function draw(){
+    //console.log("TEST");
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    for (let i = 0; i<100; i++){
+        ctx.beginPath();
+        ctx.fillStyle= "rgba(255,0,0,0.5)";
+        ctx.arc(
+            random(canvas.width),
+            random(canvas.height),
+            random(50),0,2*Math.PI,
+            );
+            ctx.fill();
+    }
+
+}
+
+
+btn.addEventListener("click", draw);
