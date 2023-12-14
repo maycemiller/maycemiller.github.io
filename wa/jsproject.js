36e3ac30eb2444e3452b01b9c3191da8fef3bb67
maycemiller <mami8235@colorado.edu>
2023-12-14 12:12:35 -0700

    final js project

--- wa/jsproject.js
+++ wa/jsproject.js
@@ -1,44 +1,61 @@
 function generatePhoneNumber() {
-    let phoneNumber = '';
-    for (let i = 0; i < 10; i++) {
-        phoneNumber += Math.floor(Math.random() * 10);
-    }
-    return phoneNumber; 
- }
- 
- document.getElementById('js-new-quote').addEventListener('click', function() {
-    document.getElementById('js-quote-text').textContent = generatePhoneNumber();
-    button.style.left = `${generateRandomPosition().x}px`;
-    button.style.top = `${generateRandomPosition().y}px`;
+   let phoneNumber = '';
+   for (let i = 0; i < 10; i++) {
+       phoneNumber += Math.floor(Math.random() * 10);
+   }
+   return phoneNumber; 
+}
+
+document.getElementById('gen-phone-number').addEventListener('click', function() {
+   document.getElementById('js-quote-text').textContent = generatePhoneNumber();
+
+});
+
+let button = document.getElementById('gen-phone-number');
+
+let genPhoneNumberButton = document.getElementById('gen-phone-number');
+
+genPhoneNumberButton.addEventListener('click', function() {
+ document.getElementById('js-quote-text').textContent = generatePhoneNumber();
+ button.style.left = `${generateRandomPosition().x}px`;
+ button.style.top = `${generateRandomPosition().y}px`;
 });
 
 let myPhoneNumberButton = document.getElementById('my-phone-number');
 
+   myPhoneNumberButton.addEventListener('click', function() {
+   document.getElementById('js-quote-text').textContent = generatePhoneNumber();
+   button.style.left = `${generateRandomPosition().x}px`;
+   button.style.top = `${generateRandomPosition().y}px`;
+   alert("Congrats, we found your phone number!");
+   });
 
-let myPhoneNumberFunction = function(){
-  document.getElementById('js-quote-text').textContent = generatePhoneNumber();
-};
 
 
-myPhoneNumberButton.addEventListener('click', function() {
-   myPhoneNumberButton.removeEventListener('click', myPhoneNumberFunction);
-   alert("Congrats on finding your phone number!");
+
+
+function generateRandomPosition() {
+   let x = Math.floor(Math.random() * (window.innerWidth - button.offsetWidth));
+   let y = Math.floor(Math.random() * (window.innerHeight - button.offsetHeight));
+   return {x, y};
+}
+
+
+document.getElementById('js-new-quote').addEventListener('click', function() {
+   document.getElementById('js-quote-text').textContent = generatePhoneNumber();
+   button.style.left = `${generateRandomPosition().x}px`;
+   button.style.top = `${generateRandomPosition().y}px`;
+
+  //  let myPhoneNumberButton = document.getElementById('my-phone-number');
+
+  //  myPhoneNumberButton.addEventListener('click', function() {
+  //  document.getElementById('js-quote-text').textContent = generatePhoneNumber();
+  //  button.style.left = `${generateRandomPosition().x}px`;
+  //  button.style.top = `${generateRandomPosition().y}px`;
+  //  });
 });
 
 
- let button = document.getElementById('js-new-quote');
- 
- function generateRandomPosition() {
-    let x = Math.floor(Math.random() * (window.innerWidth - button.offsetWidth));
-    let y = Math.floor(Math.random() * (window.innerHeight - button.offsetHeight));
-    return {x, y};
- }
-
- let genPhoneNumberButton = document.getElementById('gen-phone-number');
-
- genPhoneNumberButton.addEventListener('click', function() {
-    document.getElementById('js-quote-text').textContent = generatePhoneNumber();
- });
  
 
 
